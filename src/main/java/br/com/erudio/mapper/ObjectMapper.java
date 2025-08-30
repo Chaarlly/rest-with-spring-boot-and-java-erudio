0ccc9908fb0aaa258b32a0e7c2c6a8c140ccfd96
Luis Felipe Pereira de Lima <luisfelipeadm.ps@gmail.com>
2025-08-30 12:28:30 -0300

    Criado testes unitarios para Mapper

--- src/main/java/br/com/erudio/mapper/ObjectMapper.java
+++ src/main/java/br/com/erudio/mapper/ObjectMapper.java
@@ -10,22 +10,16 @@ public class ObjectMapper {
 
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
-    // ENTITY > DTO
-    // DTO -> ENTITY
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
     }
 
     public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
 
-        List<D> destinationsObjects = new ArrayList<D>();
-
+        List<D> destinationObjects = new ArrayList<D>();
         for (Object o : origin) {
-            destinationsObjects.add(mapper.map(origin, destination));
+            destinationObjects.add(mapper.map(o, destination));
         }
-        return destinationsObjects;
+        return destinationObjects;
     }
-
-
-
 }
